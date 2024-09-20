0e869348bf93d7f863710892762db85260e5b2bb
tusharfuse <tusharfuse1802.com>
2024-09-20 09:45:00 +0530

    My first Deployment

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask , render_template , request
+import requests
+app = Flask(__name__)
+
+
+@app.route('/')
+def show_form():
+    return render_template("index.html")
+
+@app.route('/weatherapp',methods = ['POST'])
+def get_weather_data():
+    url = "https://api.openweathermap.org/data/2.5/weather"
+
+    param = {'q':request.form.get("city"), # put id in quotation
+              'appid':request.form.get("appid"),
+              'units':request.form.get("units")
+              }
+    
+    response = requests.get(url,params= param)
+    data = response.json()
+    return f"data : {data}"
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
